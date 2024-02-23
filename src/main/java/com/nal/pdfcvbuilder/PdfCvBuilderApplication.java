c337664cadd419a75c5c8622886776947ea00cc6
nal95 <alex.nguepi@yahoo.fr>
2024-02-23 16:12:09 +0100

    first commit

---
+++ src/main/java/com/nal/pdfcvbuilder/PdfCvBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.nal.pdfcvbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfCvBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfCvBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nal/pdfcvbuilder/PdfCvBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nal.pdfcvbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfCvBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
