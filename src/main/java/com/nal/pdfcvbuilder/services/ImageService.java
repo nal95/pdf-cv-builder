a2eda1ca0ae70daac0991f7f3c1c87a9a2da31d8
nal95 <alex.nguepi@yahoo.fr>
2024-03-01 11:58:18 +0100

    storing image service

---
+++ src/main/java/com/nal/pdfcvbuilder/services/ImageService.java
@@ -0,0 +1,11 @@
+package com.nal.pdfcvbuilder.services;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ImageService {
+
+    String saveImage(MultipartFile file) throws IOException;
+}
