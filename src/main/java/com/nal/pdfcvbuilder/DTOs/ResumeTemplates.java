47a933f405713aeeb6ea6d753c829b5df4ba02b7
nal95 <alex.nguepi@yahoo.fr>
2024-03-12 11:02:25 +0100

    WIP: add some template

--- src/main/java/com/nal/pdfcvbuilder/DTOs/ResumeTemplates.java
+++ src/main/java/com/nal/pdfcvbuilder/DTOs/ResumeTemplates.java
@@ -7,6 +7,9 @@ public enum ResumeTemplates {
 
     SUPER("superTemplate/resume", "superTemplate/style.css" ),
     ERROR("error", "error/style.css" ),
+    NO("noTemplate/resume", "noTemplate/style.css" ),
+    AV("avTemplate/resume", "avTemplate/style.css" ),
+    BLUE("blueTemplate/resume", "blueTemplate/style.css" ),
     SIMPLE("simpleTemplate/resume", "simpleTemplate/style.css" );
 
     private final String templatePath;
